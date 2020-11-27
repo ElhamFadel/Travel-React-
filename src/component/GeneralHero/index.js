3b4ba273add1bc6991ad1cfb86a05b98dee7215c
ElhamFadel <elham2000Fadel@gmail.com>
2020-11-27 12:42:49 +0300

    create Generall Hero

---
+++ src/component/GeneralHero/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Header from "../../component/Header";
+import { HeroImage, OverLayComn, H1 } from "./element";
+
+function index(props) {
+  const { title, image } = props;
+  return (
+    <div className="tripPage">
+      <HeroImage src={image}>
+        <OverLayComn>
+          <Header />
+          <H1>{title}</H1>
+        </OverLayComn>
+      </HeroImage>
+    </div>
+  );
+}
+
+export default index;
--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -1,11 +1,14 @@
 import React from "react";
 import Footer from "../../component/Footer";
 import Hero from "../../componentAbout/Hero";
+import image from "../../assets/img/trip.jpeg";
+import GeneralHero from "../../component/GeneralHero";
 
 function index() {
   return (
     <div className="about">
-      <Hero />
+      {/* <Hero /> */}
+      <GeneralHero title={"Trips"} image={image} />
 
       <Footer />
     </div>
