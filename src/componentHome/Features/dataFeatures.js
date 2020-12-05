81bf20d83f1d3bddbb8a0d442464233c33d820ba
ElhamFadel <elham2000Fadel@gmail.com>
2020-12-05 14:43:50 +0300

    Edit feature component

---
+++ src/componentHome/Features/dataFeatures.js
@@ -0,0 +1,38 @@
+export const dataFeatuer = [
+  {
+    classIcon: "lnr lnr-user",
+    title: "Expert Technicians",
+    Para:
+      " Usage of the Internet is becoming more common due to rapid  advancement of technology and power.",
+  },
+  {
+    classIcon: "lnr lnr-license",
+    title: "Professional Service",
+    Para:
+      " Usage of the Internet is becoming more common due to rapid  advancement of technology and power.",
+  },
+  {
+    classIcon: "lnr lnr-phone",
+    title: "Great Support",
+    Para:
+      " Usage of the Internet is becoming more common due to rapid  advancement of technology and power.",
+  },
+  {
+    classIcon: "lnr lnr-rocket",
+    title: "Technical Skills",
+    Para:
+      " Usage of the Internet is becoming more common due to rapid  advancement of technology and power.",
+  },
+  {
+    classIcon: "lnr lnr-diamond",
+    title: "Highly Recomended",
+    Para:
+      " Usage of the Internet is becoming more common due to rapid  advancement of technology and power.",
+  },
+  {
+    classIcon: "lnr lnr-bubble",
+    title: "Positive Reviews",
+    Para:
+      " Usage of the Internet is becoming more common due to rapid  advancement of technology and power.",
+  },
+];
--- src/componentHome/Features/index.js
+++ src/componentHome/Features/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import Container from "../../component/styles/Container";
 import { RowHead } from "../../component/styles/GlobalStyle";
 import { H2, Para, H4 } from "../../component/Typography";
+import { dataFeatuer } from "./dataFeatures";
 import {
   SectionFeature,
   Row,
@@ -20,78 +21,17 @@ function Features() {
           <Para>Who are in extremely love with eco friendly system.</Para>
         </RowHead>
         <Row>
-          <Column>
-            <SingleFeature>
-              <HeadColumn>
-                <Iner className="lnr lnr-user"></Iner>
-                <H4>Expert Technicians</H4>
-              </HeadColumn>
-              <Para>
-                Usage of the Internet is becoming more common due to rapid
-                advancement of technology and power.
-              </Para>
-            </SingleFeature>
-          </Column>
-          <Column>
-            <SingleFeature>
-              <HeadColumn>
-                <Iner className="lnr lnr-license"></Iner>
-                <H4>Professional Service</H4>
-              </HeadColumn>
-              <Para>
-                Usage of the Internet is becoming more common due to rapid
-                advancement of technology and power.
-              </Para>
-            </SingleFeature>
-          </Column>
-          <Column>
-            <SingleFeature>
-              <HeadColumn>
-                <Iner className="lnr lnr-phone"></Iner>
-                <H4>Great Support</H4>
-              </HeadColumn>
-              <Para>
-                Usage of the Internet is becoming more common due to rapid
-                advancement of technology and power.
-              </Para>
-            </SingleFeature>
-          </Column>
-          <Column>
-            <SingleFeature>
-              <HeadColumn>
-                <Iner className="lnr lnr-rocket"></Iner>
-                <H4>Technical Skills</H4>
-              </HeadColumn>
-              <Para>
-                Usage of the Internet is becoming more common due to rapid
-                advancement of technology and power.
-              </Para>
-            </SingleFeature>
-          </Column>
-          <Column>
-            <SingleFeature>
-              <HeadColumn>
-                <Iner className="lnr lnr-diamond"></Iner>
-                <H4>Highly Recomended</H4>
-              </HeadColumn>
-              <Para>
-                Usage of the Internet is becoming more common due to rapid
-                advancement of technology and power.
-              </Para>
-            </SingleFeature>
-          </Column>
-          <Column>
-            <SingleFeature>
-              <HeadColumn>
-                <Iner className="lnr lnr-bubble"></Iner>
-                <H4>Positive Reviews</H4>
-              </HeadColumn>
-              <Para>
-                Usage of the Internet is becoming more common due to rapid
-                advancement of technology and power.
-              </Para>
-            </SingleFeature>
-          </Column>
+          {dataFeatuer.map((feature) => (
+            <Column>
+              <SingleFeature>
+                <HeadColumn>
+                  <Iner className={feature.classIcon}></Iner>
+                  <H4>{feature.title}</H4>
+                </HeadColumn>
+                <Para>{feature.Para}</Para>
+              </SingleFeature>
+            </Column>
+          ))}
         </Row>
       </Container>
     </SectionFeature>
