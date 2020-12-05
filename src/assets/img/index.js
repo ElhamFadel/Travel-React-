fde2e43363fee3ef69cc0ae24447c7d44eaf72b5
ElhamFadel <elham2000Fadel@gmail.com>
2020-12-05 14:56:30 +0300

    Edit GalleryImage

---
+++ src/assets/img/index.js
@@ -0,0 +1,7 @@
+export { default as Gallery1 } from "./1.jpeg";
+export { default as Gallery2 } from "./2.jpeg";
+export { default as Gallery3 } from "./3.jpeg";
+export { default as Gallery4 } from "./4.jpeg";
+export { default as Gallery5 } from "./5.jpeg";
+export { default as Gallery6 } from "./6.jpeg";
+export { default as Gallery8 } from "./8.jpeg";
